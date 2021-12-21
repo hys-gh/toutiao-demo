4e3a6a73f982949bc473ab7fe4c0e8f6a10a7f9c
hys <hys971206@163.com>
2021-12-21 10:34:13 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <router-view />
-    啊哈哈
+    <div>
+      <i class="iconfont icon-dianzan"></i>
+      <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
 </template>
 
